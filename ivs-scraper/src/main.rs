2c2a293406916b09d89d9c8966c63c0bbbcf35cc
JimLynchCodes <mrdotjim@gmail.com>
2024-05-01 23:16:11 -0400

    checking investors page for todays date

--- ivs-scraper/src/main.rs
+++ ivs-scraper/src/main.rs
@@ -1,26 +1,32 @@
-use chrono::{self, DateTime, Local};
+use chrono::{self, DateTime, Datelike, Local, Month};
 use cli_table::{print_stdout, Table, WithTitle};
 
 #[derive(Table)]
 struct Scraping {
     #[table(title = "Date Scraped")]
     date_scraped: DateTime<Local>,
+
     #[table(title = "Date Released")]
-    date_released: DateTime<Local>,
+    date_released: String,
+
     #[table(title = "New?")]
     is_new: bool,
+
     #[table(title = "Title")]
     title: String,
+
     #[table(title = "Price (text)")]
     share_price_str: String,
+
     #[table(title = "Price (number)")]
     share_price_num: f64,
+
     #[table(title = "url")]
     url: String,
 }
 
 fn main() {
-    println!("Running!");
+    println!("\nRunning!\n");
 
     let investor_relations_page_urls = vec![
         "https://www.acurxpharma.com/news-media/press-releases",
@@ -28,28 +34,80 @@ fn main() {
         // "http://www.investor.adagene.com/news-events/news-releases",  // this one doesn't work for some reason?
     ];
 
-    let date_to_search_for_v1 = "May 01, 2024";
-    let date_to_search_for_v2 = "2024.05.01";
+    let dt = Local::now();
 
-    let mut scrapings_filtered = vec![];
+    println!("Checking for: {}", dt);
+    println!("Checking for: {}", dt.naive_local());
+    println!("Checking for: {}", dt.to_utc());
+    println!("Checking for: {}", dt.year());
+    println!("Checking for: {}", dt.month());
+    println!("Checking for: {}", dt.day());
 
-    investor_relations_page_urls.into_iter().for_each(|url| {
-        let response = reqwest::blocking::get(url).unwrap().text().unwrap();
+    let month = Month::try_from(u8::try_from(dt.month()).unwrap()).ok();
 
-        if response.contains(date_to_search_for_v1) || response.contains(date_to_search_for_v2) {
-            scrapings_filtered.push(Scraping {
-                date_scraped: chrono::offset::Local::now(),
-                date_released: chrono::offset::Local::now(),
-                is_new: true,
-                title: "title".to_string(),
-                share_price_str: "$2.54".to_string(),
-                share_price_num: 2.54,
-                url: url.to_string(),
-            });
+    match month {
+        None => {
+            println!("couldn't conert to month prefix :(");
         }
-    });
+        Some(month) => {
+            println!("month is {:?}", month);
 
-    assert!(print_stdout(scrapings_filtered.with_title()).is_ok());
+            let day = dt.day();
+            let month_num = dt.month();
+            let year = dt.year();
 
-    println!("Finished!");
+            let day_with_zero_padding = if day < 10 {
+                format!("0{day}")
+            } else {
+                day.to_string()
+            };
+
+            let month_with_zero_padding = if month_num < 10 {
+                format!("0{month_num}")
+            } else {
+                month_num.to_string()
+            };
+
+            // v1, "Slash Format"         ->   YYYY/MM/DD
+            let date_to_search_for_v1 =
+                format!("{year}/{month_with_zero_padding}/{day_with_zero_padding}");
+
+            // v2, "Dots Format"          ->   YYYY.MM.DD
+            let date_to_search_for_v2 =
+                format!("{year}.{month_with_zero_padding}.{day_with_zero_padding}");
+
+            // v3, "Month Prefix Format"  ->   MMM DD, YYY
+            let date_to_search_for_v3 = format!("{:?} {day_with_zero_padding}, {year}", month);
+
+            println!("\nSearching for these dates representing today:");
+            println!("{date_to_search_for_v1}");
+            println!("{date_to_search_for_v2}");
+            println!("{date_to_search_for_v3}");
+
+            let mut scrapings_filtered = vec![];
+
+            investor_relations_page_urls.into_iter().for_each(|url| {
+                let response = reqwest::blocking::get(url).unwrap().text().unwrap();
+
+                if response.contains(&date_to_search_for_v1)
+                    || response.contains(&date_to_search_for_v2)
+                    || response.contains(&date_to_search_for_v3)
+                {
+                    scrapings_filtered.push(Scraping {
+                        date_scraped: chrono::offset::Local::now(),
+                        date_released: "?".to_string(),
+                        is_new: true,
+                        title: "title".to_string(),
+                        share_price_str: "$2.54".to_string(),
+                        share_price_num: 2.54,
+                        url: url.to_string(),
+                    });
+                }
+            });
+
+            assert!(print_stdout(scrapings_filtered.with_title()).is_ok());
+
+            println!("Finished!");
+        }
+    }
 }
